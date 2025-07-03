387a2182ad0492cd8344d06309dfff7577825272
Elvis Diniz <elvisdiniz@outlook.com>
2025-07-03 20:42:45 +0000

    test: add TestWriteZipFile for validating zip file creation and JSON content

--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"archive/zip"
+	"encoding/json"
+	"io"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -117,6 +120,85 @@ func TestWriteJSONFile(t *testing.T) {
 	// Check if the file exists
 	if _, err := os.Stat(outputFilePath); os.IsNotExist(err) {
 		t.Fatalf("expected JSON file %s to exist, but it does not", outputFilePath)
+	}
+}
+
+func TestWriteZipFile(t *testing.T) {
+	// Create a temporary directory for the test file
+	tempDir := t.TempDir()
+	infilePath := filepath.Join(tempDir, "test-file.xlsx")
+	outputZipPath := filepath.Join(tempDir, "test-file.zip")
+	jsonFileName := "test-file.json" // The name of the JSON file inside the zip
+
+	// Create a sample output
+	expectedOutput := Output{
+		Metadados: Metadados{
+			Data:            "2025-07-03",
+			DataAtualizacao: "2025-07-04",
+			Observacoes:     []string{"Observação 1", "Observação 2"},
+		},
+		Medicamentos: []Medicamento{
+			{"SUBSTÂNCIA": "IBUPROFENO", "CNPJ": "12.345.678/0001-90"},
+			{"SUBSTÂNCIA": "PARACETAMOL", "CNPJ": "98.765.432/0001-10"},
+		},
+		Laboratorios: map[string]string{
+			"12.345.678/0001-90": "LAB A",
+			"98.765.432/0001-10": "LAB B",
+		},
+		Apresentacoes: []string{"COM REV", "GOTAS"},
+	}
+
+	// Write the JSON file (which is now zipped)
+	if err := writeZipFile(expectedOutput, infilePath); err != nil {
+		t.Fatalf("writeJSONFile failed: %v", err)
+	}
+
+	// Check if the zip file exists
+	if _, err := os.Stat(outputZipPath); os.IsNotExist(err) {
+		t.Fatalf("expected zip file %s to exist, but it does not", outputZipPath)
+	}
+
+	// Open the zip file
+	r, err := zip.OpenReader(outputZipPath)
+	if err != nil {
+		t.Fatalf("failed to open zip file: %v", err)
+	}
+	defer r.Close()
+
+	// Find the JSON file inside the zip
+	var jsonFile *zip.File
+	for _, f := range r.File {
+		if f.Name == jsonFileName {
+			jsonFile = f
+			break
+		}
+	}
+
+	if jsonFile == nil {
+		t.Fatalf("JSON file %s not found inside the zip archive", jsonFileName)
+	}
+
+	// Read the content of the JSON file
+	rc, err := jsonFile.Open()
+	if err != nil {
+		t.Fatalf("failed to open JSON file in zip: %v", err)
+	}
+	defer rc.Close()
+
+	jsonData, err := io.ReadAll(rc)
+	if err != nil {
+		t.Fatalf("failed to read JSON data from zip: %v", err)
+	}
+
+	// Unmarshal the JSON data
+	var actualOutput Output
+	if err := json.Unmarshal(jsonData, &actualOutput); err != nil {
+		t.Fatalf("failed to unmarshal JSON data: %v", err)
+	}
+
+	// Compare the unmarshaled data with the expected output
+	if !reflect.DeepEqual(actualOutput, expectedOutput) {
+		t.Errorf("unexpected output. got %+v, want %+v", actualOutput, expectedOutput)
 	}
 }
 
